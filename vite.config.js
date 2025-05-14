f77c4dddc9c21b6337306f9a615a6607d70b021c
ibrahimqudry <ibrahimqudry@gmail.com>
2025-05-14 20:09:54 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: process.env.NODE_ENV === 'production'
-  ? 'https://ibrahimqudry.github.io/Pharaohs/'
-  : '/'
 })
 
